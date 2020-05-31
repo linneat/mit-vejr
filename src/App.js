b9e76c1ce9498fb8f96f77064d2f936d5cad59b6
linneat <51674082+linneat@users.noreply.github.com>
2020-05-31 14:55:00 +0200

    home page with hard coded data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap">
+      <h1 className="heading">The weather for today in your city!</h1>
+      <div className="info">
+        <div className="temp">avg: 20C</div>
+        <div className="temp">min: 18C</div>
+        <div className="temp">max: 22C</div>
+        <div className="temp">feels like: 20C</div>
+      </div>
+      <div className="info">
+        <div className="city">Vanløse</div>
+        <div className="description">Clear sky</div>
+      </div>
     </div>
   );
 }
