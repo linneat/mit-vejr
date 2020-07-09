dae60af278e161b40e5574416ca0913df2fbb2c7
linneat <51674082+linneat@users.noreply.github.com>
2020-07-09 10:18:41 +0200

    css changes home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,20 +5,16 @@ export default class HomePage extends Component {
   render() {
     return (
       <div className="appWrap">
-        <h1 className="heading">
-          Compare weather in your city and anywhere else!
-        </h1>
-        <div className="buttonWrapper">
-        <Link to={"/comparetemp"} className="buttonHomePage">
-          <button>
-            Compare weather from two cities
-          </button>
-        </Link>
-        <Link to={"/comparetempmultiple"} className="buttonHomePage">
-          <button>
-            Compare feels-like temperatures from multiple cities
-          </button>
-        </Link>
+        <div className="homePageWrap">
+          <h1 className="heading">Compare weather cities around the world</h1>
+          <div className="buttonWrapper">
+            <Link to={"/comparetemp"} className="buttonHomePage">
+              <button className="buttonClass">Compare weather from two cities</button>
+            </Link>
+            <Link to={"/comparetempmultiple"} className="buttonHomePage">
+              <button className="buttonClass">Compare weather from multiple cities</button>
+            </Link>
+          </div>
         </div>
       </div>
     );
